5e1baec485363404d9df166e97f9f22d11de7393
Daniël Beeke <d.beeke@oxilion.nl>
2020-10-25 16:05:19 +0100

    Adding docs

--- docs/Helpers.js
+++ docs/Helpers.js
@@ -1,12 +1,10 @@
-import {Hole} from 'uhtml'
-import { icon } from '@fortawesome/fontawesome-svg-core'
-
+import {Hole} from "/web_modules/uhtml.js";
+import {icon} from "/web_modules/@fortawesome/fontawesome-svg-core.js";
 class FaIcon extends Hole {
-  constructor(icon) {
-    super('svg', [icon], []);
+  constructor(icon2) {
+    super("svg", [icon2], []);
   }
 }
-
-export function fa (iconInput) {
-  return new FaIcon(icon(iconInput).html[0])
+export function fa(iconInput) {
+  return new FaIcon(icon(iconInput).html[0]);
 }
--- docs/app.js
+++ docs/app.js
@@ -1,42 +1,37 @@
-import { loadStripe } from '@stripe/stripe-js'
-import Products from './cards.js'
-import { fa } from './Helpers.js'
-import { faShoppingCart, faTimes, faChevronRight } from '@fortawesome/free-solid-svg-icons'
-
-import { render, html } from 'uhtml'
-
-const shopId = 'pk_test_51HDxVgDM7H4MIyD8kbH9mdvrHgW1V0o45wDhb15zM6b55DZP2mLeebWFaRUBr0NDCfQw0KHijFhxd1HKv4gXkTam001v7tho4R'
-
+import {loadStripe} from "/web_modules/@stripe/stripe-js.js";
+import Products from "./cards.js";
+import {fa} from "./Helpers.js";
+import {faShoppingCart, faTimes, faChevronRight} from "/web_modules/@fortawesome/free-solid-svg-icons.js";
+import {render, html} from "/web_modules/uhtml.js";
+const shopId = "pk_test_51HDxVgDM7H4MIyD8kbH9mdvrHgW1V0o45wDhb15zM6b55DZP2mLeebWFaRUBr0NDCfQw0KHijFhxd1HKv4gXkTam001v7tho4R";
 class App {
   constructor() {
-    this.products = Products
+    this.products = Products;
     for (const product of this.products) {
-      product.zoom = false
+      product.zoom = false;
     }
-    this.basket = new Map()
-    this.appElement = document.querySelector('#app')
-    this.currencyFormat = new Intl.NumberFormat('nl-NL', { style: 'currency', currency: 'EUR' })
-
-    window.addEventListener('hashchange', () => {
-      this.draw()
-    })
-
-    this.draw()
+    this.basket = new Map();
+    this.appElement = document.querySelector("#app");
+    this.currencyFormat = new Intl.NumberFormat("nl-NL", {
+      style: "currency",
+      currency: "EUR"
+    });
+    window.addEventListener("hashchange", () => {
+      this.draw();
+    });
+    this.draw();
   }
-
-  draw () {
-    let method = 'template' + location.hash.substr(1, 1).toUpperCase() + location.hash.substr(2)
+  draw() {
+    let method = "template" + location.hash.substr(1, 1).toUpperCase() + location.hash.substr(2);
     if (!this[method]) {
-      method = 'template'
-      location.hash = ''
+      method = "template";
+      location.hash = "";
     }
-    render(this.appElement, this[method]())
+    render(this.appElement, this[method]());
   }
-
-  template () {
-    const zoomedProduct = this.products.find(product => product.zoom)
-    document.body.dataset.zoom = !!zoomedProduct
-
+  template() {
+    const zoomedProduct = this.products.find((product) => product.zoom);
+    document.body.dataset.zoom = !!zoomedProduct;
     return html`
       <h1 class="site-title">Kerstkaarten 2020</h1>
       
@@ -44,97 +39,91 @@ class App {
       
       ${zoomedProduct ? html`
         <img onclick="${() => {
-          zoomedProduct.zoom = false
-          this.draw()
-        }}" class="zoomed-product" src="${'images/' + zoomedProduct.image}">
-      ` : ''}
+      zoomedProduct.zoom = false;
+      this.draw();
+    }}" class="zoomed-product" src="${"images/" + zoomedProduct.image}">
+      ` : ""}
       
       <div class="cards">
-        ${this.products.map(product => {
-          const lineItem = this.basket.get(product)
-          
-          return html`
-          <div class="${'card' + (lineItem ? ' has-line-item' : '')}">
+        ${this.products.map((product) => {
+      const lineItem = this.basket.get(product);
+      return html`
+          <div class="${"card" + (lineItem ? " has-line-item" : "")}">
             <h3 class="title">${product.title}</h3>
 
             <img onclick="${() => {
-              product.zoom = !product.zoom
-              this.draw()
-            }}" class="image" src="${'images/' + product.image}">
+        product.zoom = !product.zoom;
+        this.draw();
+      }}" class="image" src="${"images/" + product.image}">
            
             <div class="add-to-basket">
               <span class="price">${this.currencyFormat.format(lineItem ? product.price * lineItem.quantity : product.price)}</span>
 
               ${lineItem ? html`
                 <button class="remove-product-button no-button" onclick="${() => {
-                  this.basket.delete(product)
-                  this.draw()
-                }}">${fa(faTimes)}</button>
-              ` : ''}
+        this.basket.delete(product);
+        this.draw();
+      }}">${fa(faTimes)}</button>
+              ` : ""}
 
               <button class="add-product-button no-button" onclick="${() => {
-                this.increaseQuantityForProduct(product)
-                this.draw()
-              }}">
-                ${lineItem?.quantity ? html`<span class="quantity">${lineItem.quantity}</span>` : ''}
+        this.increaseQuantityForProduct(product);
+        this.draw();
+      }}">
+                ${lineItem?.quantity ? html`<span class="quantity">${lineItem.quantity}</span>` : ""}
                 ${fa(faShoppingCart)}
               </button>
 
             </div>
           </div>
-        `      
-        })}       
+        `;
+    })}       
       </div>
       
-      <button class="${'go-to-stripe-button' + (!this.totalPrice() ? ' disabled' : '')}" onclick="${() => this.checkout()}">
-        ${this.totalPrice() ? html`<span class="total-price">${this.currencyFormat.format(this.totalPrice())}</>` : ''}
+      <button class="${"go-to-stripe-button" + (!this.totalPrice() ? " disabled" : "")}" onclick="${() => this.checkout()}">
+        ${this.totalPrice() ? html`<span class="total-price">${this.currencyFormat.format(this.totalPrice())}</>` : ""}
         <span class="text">Verder naar afrekenen</span>
         ${fa(faChevronRight)}
       </button>
-    `
+    `;
   }
-
-  templateSuccess () {
-    return html`<h1>Woop</h1>`
+  templateSuccess() {
+    return html`<h1>Woop</h1>`;
   }
-
-  increaseQuantityForProduct (product) {
-    let lineItem = this.basket.get(product)
-    if (!lineItem) lineItem = { quantity: 0 }
-    lineItem.quantity++
-    this.basket.set(product, lineItem)
+  increaseQuantityForProduct(product) {
+    let lineItem = this.basket.get(product);
+    if (!lineItem)
+      lineItem = {
+        quantity: 0
+      };
+    lineItem.quantity++;
+    this.basket.set(product, lineItem);
   }
-
-  totalPrice () {
-    let total = 0
+  totalPrice() {
+    let total = 0;
     Array.from(this.basket.entries()).forEach(([product, lineItem]) => {
-      total += product.price * lineItem.quantity
-    })
-    return total
+      total += product.price * lineItem.quantity;
+    });
+    return total;
   }
-
-  createLineItems () {
+  createLineItems() {
     return Array.from(this.basket.entries()).map(([product, lineItem]) => {
-      return { price: product.id, quantity: lineItem.quantity }
-    })
+      return {
+        price: product.id,
+        quantity: lineItem.quantity
+      };
+    });
   }
-
-  async checkout () {
+  async checkout() {
     const stripe = await loadStripe(shopId);
-
     stripe.redirectToCheckout({
       lineItems: this.createLineItems(),
-      mode: 'payment',
-      successUrl: location.protocol + '//' + location.host + '#success',
-      cancelUrl: location.protocol + '//' + location.host + '#cancel',
-      billingAddressCollection: 'required',
-    }).then(function (result) {
-      // If `redirectToCheckout` fails due to a browser or network
-      // error, display the localized error message to your customer
-      // using `result.error.message`.
+      mode: "payment",
+      successUrl: location.protocol + "//" + location.host + "#success",
+      cancelUrl: location.protocol + "//" + location.host + "#cancel",
+      billingAddressCollection: "required"
+    }).then(function(result) {
     });
-
   }
 }
-
-new App()
+new App();
