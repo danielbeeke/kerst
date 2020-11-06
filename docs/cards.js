0b2970f9cad125a3c923aee505e5eed805f916ba
Daniel Beeke <mail@danielbeeke.nl>
2020-11-06 20:23:25 +0100

    putting it on kerst.wilmavis.nl

--- docs/cards.js
+++ docs/cards.js
@@ -1,41 +1,50 @@
-export default [{
-  title: "Jezus, licht der wereld",
-  image: "Jezus-licht-der-wereld.jpg",
-  price: 1.5,
-  id: "price_1Hfqx1DM7H4MIyD8ZYKwdOna"
-}, {
-  title: "Blaadjes op zwart",
-  image: "blaadjes-op-zwart.jpg",
-  price: 1.5,
-  id: "price_1Hfr6pDM7H4MIyD8DzIEDkaA"
-}, {
-  title: "Let it snow",
-  image: "let-it-snow.jpg",
-  price: 1.5,
-  id: "price_1Hfr6pDM7H4MIyD8DzIEDkaA"
-}, {
-  title: "Liefde, licht en leven",
-  image: "liefde-licht-en-leven.jpg",
-  price: 1.5,
-  id: "price_1Hfr6pDM7H4MIyD8DzIEDkaA"
-}, {
-  title: "Merry Christmas",
-  image: "merry-christmas.jpg",
-  price: 1.5,
-  id: "price_1Hfr6pDM7H4MIyD8DzIEDkaA"
-}, {
-  title: "Sneeuw landschap",
-  image: "sneeuw-landschap.jpg",
-  price: 1.5,
-  id: "price_1Hfr6pDM7H4MIyD8DzIEDkaA"
-}, {
-  title: "Winter flowers",
-  image: "winter-flowers.jpg",
-  price: 1.5,
-  id: "price_1Hfr6pDM7H4MIyD8DzIEDkaA"
-}, {
-  title: "Wolkenfabriek",
-  image: "wolkenfabriek.jpg",
-  price: 1.5,
-  id: "price_1Hfr6pDM7H4MIyD8DzIEDkaA"
-}];
+export default [
+  {
+    "title": "Jezus, licht der wereld",
+    "image": "Jezus-licht-der-wereld.jpg",
+    "price": 1.5,
+    "id": "price_1HkaRDDM7H4MIyD8LmoI5FKm"
+  },
+  {
+    "title": "Blaadjes op zwart",
+    "image": "blaadjes-op-zwart.jpg",
+    "price": 1.5,
+    "id": "price_1HkaRDDM7H4MIyD8LmoI5FKm"
+  },
+  {
+    "title": "Let it snow",
+    "image": "let-it-snow.jpg",
+    "price": 1.5,
+    "id": "price_1Hfr6pDM7H4MIyD8DzIEDkaA"
+  },
+  {
+    "title": "Liefde, licht en leven",
+    "image": "liefde-licht-en-leven.jpg",
+    "price": 1.5,
+    "id": "price_1Hfr6pDM7H4MIyD8DzIEDkaA"
+  },
+  {
+    "title": "Merry Christmas",
+    "image": "merry-christmas.jpg",
+    "price": 1.5,
+    "id": "price_1Hfr6pDM7H4MIyD8DzIEDkaA"
+  },
+  {
+    "title": "Sneeuw landschap",
+    "image": "sneeuw-landschap.jpg",
+    "price": 1.5,
+    "id": "price_1Hfr6pDM7H4MIyD8DzIEDkaA"
+  },
+  {
+    "title": "Winter flowers",
+    "image": "winter-flowers.jpg",
+    "price": 1.5,
+    "id": "price_1Hfr6pDM7H4MIyD8DzIEDkaA"
+  },
+  {
+    "title": "Wolkenfabriek",
+    "image": "wolkenfabriek.jpg",
+    "price": 1.5,
+    "id": "price_1HkaRDDM7H4MIyD8LmoI5FKm"
+  }
+]
