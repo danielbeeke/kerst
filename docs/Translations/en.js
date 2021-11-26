576d4462b88b2c86e398d40deec9585ddc42f5e3
Daniel Beeke <mail@danielbeeke.nl>
2021-11-26 13:04:24 +0100

    changed text

--- docs/Translations/en.js
+++ docs/Translations/en.js
@@ -12,6 +12,11 @@ export const Translations = {
   'Niet meer<br>beschikbaar': 'Not<br>available',
   'Nog <strong>{stock}</strong> op<br>voorraad': 'Only <strong>{stock}</strong> left<br>in stock',
   'We gaan met uw bestelling aan de slag, ook hebben we een e-mail met de order bevestiging gestuurd.': 'We will start to process your order, we have also send you a confirmation mail.',
-  'Wat is er nu leuker dan een mooie kaart op de mat valt? Speciaal voor kerst of gewoon zomaar. Juist nu we niet zo veel samen kunnen zijn kan een kaartje extra goed doen! <ul><li>Formaat: A6, 148 x 105 mm</li><li>350 grams papier</li><li>Full color print</li><li>Ongevouwen met adreslijnen op de achterkant</li></ul>Bij elke kaart zit een kraft envelop van 100% gerecycled papier.': 'What is nicer than a beautiful postcard landing on your doormat? Perfect for Christmas or just for fun. Especially now that we cannot be together that much, a postcard can do extra good!  <ul><li>Format: A6, 148 x 105 mm</li><li>350 grams paper</li><li>Full color print</li><li>Unfolded with address lines on the back side.</li></ul>Every card has a kraft envelope made from 100% recycled paper.',
+  'Wat is er nu leuker dan een mooie kaart op de mat valt? Speciaal voor kerst of gewoon zomaar. Juist nu we niet zo veel samen kunnen zijn kan een kaartje extra goed doen!<br><br>Het formaat van de kaarten is 148 x 105 mm.<br>Inclusief kraft envelop van 100% gerecycled papier.<br><br>Prijzen: 5 voor €9.50 | 10 voor €18.50 | 15 voor €27 | 20 voor €35': `What is nicer than a beautiful postcard landing on your doormat? Perfect for Christmas or just for fun. Especially now that we cannot be together that much, a postcard can do extra good!
+
+  Size: A6, 148 x 105 mm.
+  Every card comes with a craft envelope made from 100% recycled paper.
+  
+  Prices: 5 for €9.50 | 10 for €18.50 | 15 for €27 | 20 for €35`,
   'Prijzen:<br>5 voor €9.50<br>10 voor €18.50<br>15 voor €27<br>20 voor €35': 'Prices:<br>5 for €9.50<br>10 for €18.50<br>15 for €27<br>20 for €35'
 }
