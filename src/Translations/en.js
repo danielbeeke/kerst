c91e12a57d103526ce7203e8d4cc20672bb95a4c
Daniel Beeke <mail@danielbeeke.nl>
2022-12-03 14:18:58 +0100

    changes

--- src/Translations/en.js
+++ src/Translations/en.js
@@ -1,7 +1,7 @@
 export const Translations = {
   'Terug naar het overzicht': 'Back to overview',
   'Nieuw': 'New',
-  'Kerstkaarten': 'Christmas postcards',
+  'Kerstkaarten': 'Christmas cards',
   'Oeps er ging iets mis met de bestelling.': 'Oops something went wrong with your order.',
   'Probeer het nog eens opnieuw.': 'Maybe try it again.',
   'Bedankt voor de bestelling.': 'Thank you for your order.',
@@ -13,6 +13,23 @@ export const Translations = {
   'Niet meer<br>beschikbaar': 'Not<br>available',
   'Nog <strong>{stock}</strong> op<br>voorraad': 'Only <strong>{stock}</strong> left<br>in stock',
   'We gaan met uw bestelling aan de slag, ook hebben we een e-mail met de order bevestiging gestuurd.': 'We will start to process your order, we have also send you a confirmation mail.',
-  'Wat is er nu leuker dan een mooie kaart die op de mat valt? Speciaal voor kerst, nieuwjaar of gewoon zomaar. <br><br>Het formaat van de kaarten is 148 x 105 mm.<br>Inclusief kraft envelop van 100% gerecycled papier.<br><br>Prijzen: 5 voor €9.50 | 10 voor €18.50 | 15 voor €27 | 20 voor €35': `What is nicer than a beautiful postcard landing on your doormat? Perfect for Christmas, New Year or just for fun. <br><br>Size: A6, 148 x 105 mm.<br>Every card comes with a craft envelope made from 100% recycled paper.<br><br>Prices: 5 for €9.50 | 10 for €18.50 | 15 for €27 | 20 for €35`,
+  'introduction': `<p>
+What is nicer than a beautiful postcard landing on your doormat?
+These colorful cards are perfect to send as a Christmas card or to wish someone a happy new year.
+  </p>
+
+  <strong>Details</strong><ul>
+      <li>A6 format (148 x 105 mm)</li>
+      <li>350 grams paper</li>
+      <li>Full color print</li>
+      <li>- Unfolded with address lines on the back </li>
+      <li>Comes with a 100% recycled craft envelope</li>
+  </ul>
+
+<p>If you have questions, please don't hesitate to contact me! :)</p>
+<p>
+<a target="_blank" class="instagram" href="https://www.instagram.com/wilmavis">WilmaVis</a>
+
+</p>`,
   'Prijzen:<br>5 voor €9.50<br>10 voor €18.50<br>15 voor €27<br>20 voor €35': 'Prices:<br>5 for €9.50<br>10 for €18.50<br>15 for €27<br>20 for €35'
 }
--- src/Translations/nl.js
+++ src/Translations/nl.js
@@ -1,1 +1,20 @@
-export const Translations = {}
+export const Translations = {
+    'introduction': `<p>
+    Wat is er nu leuker dan een mooie kaart op de mat? Speciaal voor kerst, om iemand een gelukkig nieuwjaar te wensen, of gewoon zomaar.
+    </p>
+
+    <strong>Details</strong><ul>
+        <li>A6 formaat (148 x 105 mm)</li>
+        <li>350 grams papier</li>
+        <li>Full color print</li>
+        <li>Ongevouwen met adreslijnen op de achterkant</li>
+        <li>Bij elke kaart zit een kraft envelop van 100% gerecycled papier</li>
+    </ul>
+
+<p>Laat me weten als je nog vragen hebt, ik help je graag! :)</p>
+<p>
+<a target="_blank" class="instagram" href="https://www.instagram.com/wilmavis">WilmaVis</a>
+
+</p>
+`
+}
